5764560f80d0006214a0f1220b258ef022490422
jituyadaav255-cpu <jituyadaav255@gmail.com>
2026-05-19 17:17:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os
 from pyrogram import Client
 
-# यहाँ से टोकन सीधे Railway के Variables से उठाएगा
+# यहाँ हम सीधे Railway की सेटिंग से TOKEN उठा रहे हैं
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
+# Client सेटअप
 app = Client("my_bot", bot_token=BOT_TOKEN)
 
 print("बोट लाइव हो रहा है...")
