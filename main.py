48d48a697ebee5b7ed0329e53cde65c42a6d216c
jituyadaav255-cpu <jituyadaav255@gmail.com>
2026-05-19 15:39:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import asyncio
+import nest_asyncio
 from pyrogram import Client
 from modules.vars import API_ID, API_HASH, BOT_TOKEN
+
+# यह लाइन एरर को फिक्स करेगी
+nest_asyncio.apply()
 
 app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 async def main():
     await app.start()
     print("बोट सफलता के साथ लाइव हो गया है!")
-    await asyncio.Event().wait()
+    # बोट को चालू रखने के लिए
+    await asyncio.Future()
 
 if __name__ == "__main__":
     asyncio.run(main())
