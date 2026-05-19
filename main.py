d1a7232224525c285b3eade7f7465b4f7231fec1
jituyadaav255-cpu <jituyadaav255@gmail.com>
2026-05-19 17:06:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-from pyrogram import Client
+from pyrogram import Client, idle
 from vars import BOT_TOKEN
 
-# बस BOT_TOKEN का इस्तेमाल करें, ID और HASH की जरूरत नहीं है
+# Client सेटअप
 app = Client("my_bot", bot_token=BOT_TOKEN)
+
+async def main():
+    await app.start()
+    print("बोट सफलता के साथ लाइव हो गया है!")
+    # यह बोट को टेलीग्राम पर एक्टिव रखता है ताकि वो मैसेजेस सुन सके
+    await idle() 
+
+if __name__ == "__main__":
+    app.run(main())
