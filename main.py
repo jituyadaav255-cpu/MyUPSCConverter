043104a55436dd547704bdbd636d4bef30a5a1b3
jituyadaav255-cpu <jituyadaav255@gmail.com>
2026-05-19 17:13:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from pyrogram import Client
 from vars import BOT_TOKEN
 
-# क्लाइंट को इनिशियलाइज़ करें
-app = Client("my_bot", bot_token=BOT_TOKEN)
+# API_ID और HASH को None रखें ताकि सिर्फ टोकन का उपयोग हो
+app = Client(
+    "my_bot",
+    api_id=None,
+    api_hash=None,
+    bot_token=BOT_TOKEN
+)
 
-# यह Pyrogram का अपना तरीका है बोट को चलाने का
-print("बोट लाइव हो रहा है...")
+print("बोट शुरू हो रहा है...")
 app.run()
