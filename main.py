2eb0d5deb849c0b7afb21f38aa419c4e90da49ce
jituyadaav255-cpu <jituyadaav255@gmail.com>
2026-05-19 15:48:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 import asyncio
-# सबसे पहले nest_asyncio अप्लाई करें
 import nest_asyncio
 nest_asyncio.apply()
 
-# अब Pyrogram को इंपोर्ट करें
 from pyrogram import Client
-from modules.vars import API_ID, API_HASH, BOT_TOKEN
+# यहाँ से modules. हटा दिया है, क्योंकि vars.py सीधे रूट फोल्डर में है
+from vars import API_ID, API_HASH, BOT_TOKEN
 
 app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 async def main():
     await app.start()
     print("बोट सफलता के साथ लाइव हो गया है!")
-    # बोट को चालू रखने के लिए
     await asyncio.Future()
 
 if __name__ == "__main__":
